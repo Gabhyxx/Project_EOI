3aa1cdf45f1b27bba8566e67d9a03dfb507b54ef
JotaJueves <jjpra196@gmail.com>
2023-10-03 14:11:01 +0200

    Heartbeat animation implemented

--- Assets/Scripts/HeartbeatScroll.cs
+++ Assets/Scripts/HeartbeatScroll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeartbeatScroll : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class HeartbeatScroll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetComponent<MeshRenderer>().material.mainTextureOffset = new Vector2(Time.time, 0);
         //transform.Translate(Vector3.left * speed);
     }
 }
