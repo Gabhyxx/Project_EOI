2072532eda6a324937037d6b30077f08fd9920c1
JotaJueves <jjpra196@gmail.com>
2023-10-12 11:27:03 +0200

    Pistola de pulsos pierde municion al disparar y se recarga sola

--- Assets/Scripts/ProjectileWeapon.cs
+++ Assets/Scripts/ProjectileWeapon.cs
@@ -11,23 +11,46 @@ public class ProjectileWeapon : MonoBehaviour
     public GameObject pistolProjectile;
     public Transform shootingTransform;
     public float cadency;
+    public int chargedAmmo = 20;
+    public int maxAmmo = 20;
     //Variables Privadas
     GameObject cloneProjectile;
     float timeLastShoot;
+    int timePassed = 0;
 
     private void Start()
     {
         cadency = 60 / cadency;
     }
+    private void Update()
+    {
+        timePassed++;
+        RechargingPistol();
+        if (Input.GetMouseButton(0))
+        {
+            CancelInvoke();
+        }
+    }
     public void ProjectileCreation()
     {
-        if(Time.time > timeLastShoot + cadency)
+        if(Time.time > timeLastShoot + cadency && chargedAmmo > 0)
         {
+            chargedAmmo--;
             cloneProjectile = Instantiate(pistolProjectile, shootingTransform.position, shootingTransform.rotation);
             cloneProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * projectileForce);
             Destroy(cloneProjectile, destroyTime);
             timeLastShoot = Time.time;
         }
-        
+    }
+    public void RechargingPistol()
+    {
+        Invoke("Reloading", 3.0f);
+    }
+    public void Reloading()
+    {
+        if(chargedAmmo < maxAmmo && timePassed % 15f == 0)
+        {
+            chargedAmmo++;
+        }
     }
 }
